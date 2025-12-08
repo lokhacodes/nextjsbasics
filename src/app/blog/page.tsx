12d4578d23deb0fcacbd738ff751d9c2ea7d6aaf
lokhacodes <sumaiyasonali421@gmail.com>
2025-12-08 14:09:27 +0600

    loading.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
     },
 };
 
-export default function Blog() {
+export default async function Blog() {
+    await new Promise((resolve) => setTimeout (resolve, 2000));
     return <h1>Welcome to Blog Page</h1>
 }
